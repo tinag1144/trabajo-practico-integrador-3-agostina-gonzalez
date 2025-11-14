0a2a21c2ee81449aeb301d56748bb1b992ff4326
unknown <sogazelaznog@gmail.com>
2025-11-14 12:04:52 -0300

    Footer

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+export const Footer = () => {
+
+  return (
+    <footer className="bg-dark text-white py-3 mt-auto">
+      <div className="container text-center">
+        <p className="mb-0 fw-light">
+          © 2025 - TP Integrador III — Desarrollado por Agostina González
+        </p>
+      </div>
+    </footer>   
+  );
+};
